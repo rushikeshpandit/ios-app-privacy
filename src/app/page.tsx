6ebf47f4f62ceaf21de579889281c368be2ae7fb
Rushikesh Pandit <13253098+rushikeshpandit@users.noreply.github.com>
2024-04-15 16:34:28 +0530

    1.2 UI enhancement (#3)
    * - Updated keyname in datasource
    
    * - WIP on UI enhancement
    
    * - Added heroicons
    
    * - Added selected key
    
    * - Handling case for required reasons api
    
    * - Updated datasource keys
    
    * - Completed UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,59 @@
 "use client";
+import { Disclosure } from "@headlessui/react";
+import { ChevronUpIcon } from "@heroicons/react/20/solid";
+import { Checkbox } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 import datasource from "./datasource.json";
-import { useState } from "react";
 import MainHint from "./MainHint";
 import SubHint from "./SubHint";
-import { Checkbox } from "@nextui-org/react";
 
+interface reason {
+	id: string;
+	key: string;
+	value: string;
+	selected: boolean;
+}
+
+interface api_type {
+	id: string;
+	value: string;
+	type: string;
+	reasons: reason[];
+}
 interface ds {
 	id: string;
 	label: string;
 	hint: string;
-	api_type?:
-		| {
-				id: string;
-				value: string;
-				type: string;
-				Reasons: { id: string; key: string; value: string }[];
-		  }[]
-		| any;
+	api_type?: api_type[] | any;
 	btn_title?: any | undefined;
 	purpose?: any | undefined;
+}
+
+interface options {
+	id: string;
+	value: string;
+	label: string;
+	selected: boolean;
+}
+
+interface options_type {
+	id: string;
+	label: string;
+	options: options[];
+}
+
+interface intension_type {
+	id: string;
+	value: string;
+	label: string;
+	selected: boolean;
+}
+
+interface collected_type {
+	name?: string | undefined;
+	option?: options | undefined;
+	purpose?: intension_type[] | undefined;
+	intension?: intension_type[] | undefined;
 }
 
 export default function Home() {
@@ -27,6 +62,33 @@ export default function Home() {
 	const [subHintID, setSubHintID] = useState("");
 	const [privacyTrackingEnabled, setPrivacyTrackingEnabled] = useState(false);
 	const [trackingDomains, setTrackingDomains] = useState([{ domain: "" }]);
+	const [reasons_final_value, setReasons_final_value] = useState<api_type[]>(
+		[]
+	);
+	const [collected_data_types, setCollected_data_type] = useState<
+		collected_type[]
+	>([]);
+
+	useEffect(() => {
+		const val = datasource[2].api_type as api_type[];
+		setReasons_final_value(val);
+
+		const collect = datasource[3].api_type as options_type[];
+		const collected_data_types_final = collect.map((api: options_type) => {
+			return api.options?.map((option) => {
+				return {
+					name: api.label,
+					option: option,
+					intension: datasource[3].intension,
+					purpose: datasource[3].purpose,
+				};
+			});
+		});
+
+		setCollected_data_type(
+			collected_data_types_final.flat(Infinity) as collected_type[]
+		);
+	}, []);
 
 	let handleChange = (i: number, e: any) => {
 		let newTrackingDomains = [...trackingDomains];
@@ -35,9 +97,7 @@ export default function Home() {
 	};
 
 	let addTrackingDomains = () => {
-		let newTrackingDomains = [...trackingDomains];
-		newTrackingDomains.push({ domain: "" });
-		setTrackingDomains(newTrackingDomains);
+		setTrackingDomains([...trackingDomains, { domain: "" }]);
 	};
 
 	let removeTrackingDomains = (i: number) => {
@@ -46,9 +106,60 @@ export default function Home() {
 		setTrackingDomains(newTrackingDomains);
 	};
 
+	let downloadFile = () => {
+		// Handle download file here
+	};
+
+	let updateCollectedDataTypeIntension = (
+		ct: collected_type,
+		id: string,
+		is_purpose: boolean
+	) => {
+		if (is_purpose) {
+			const updated_cdt = collected_data_types.map((cdt) => {
+				if (cdt.option?.id === ct.option?.id) {
+					const updated_purpose = cdt.purpose!.map((purpose) => {
+						if (purpose.id === id) {
+							return { ...purpose, selected: !purpose.selected };
+						}
+						return purpose;
+					});
+					return {
+						...cdt,
+						purpose: updated_purpose,
+						option: { ...cdt.option, selected: !cdt.option!.selected },
+					};
+				}
+				return cdt;
+			});
+
+			setCollected_data_type(updated_cdt as collected_type[]);
+		} else {
+			const updated_cdt = collected_data_types.map((cdt) => {
+				if (cdt.option?.id === ct.option?.id) {
+					const updated_intension = cdt.intension?.map((intension) => {
+						if (intension.id === id) {
+							return { ...intension, selected: !intension.selected };
+						}
+						return intension;
+					});
+					return {
+						...cdt,
+						intension: updated_intension,
+						option: { ...cdt.option, selected: !cdt.option!.selected },
+					};
+				}
+				return cdt;
+			});
+
+			setCollected_data_type(updated_cdt as collected_type[]);
+		}
+	};
+
 	function toggle() {
 		setshouldShowMainHint((shouldShowMainHint) => !shouldShowMainHint);
 	}
+
 	function enableMainHint() {
 		return <MainHint shouldShowMainHint={shouldShowMainHint} toggle={toggle} />;
 	}
@@ -63,6 +174,25 @@ export default function Home() {
 		setSubHintID(id);
 		setshouldShowSubHint(!shouldShowSubHint);
 	}
+
+	function updateApiTypes(api_id: string, reason_id: string) {
+		var api_to_update = reasons_final_value.find((o) => o.id === api_id);
+		var index_to_replace = reasons_final_value.findIndex(
+			(o) => o.id === api_id
+		);
+		var reason_to_update = api_to_update?.reasons.find(
+			(o) => o.id === reason_id
+		);
+		reason_to_update!.selected = !reason_to_update!.selected;
+		var updated_reasons = api_to_update?.reasons.map((o) =>
+			o.id === reason_to_update?.id ? reason_to_update : o
+		);
+		api_to_update!.reasons = updated_reasons!;
+		let a_t = [...reasons_final_value];
+		a_t[index_to_replace] = api_to_update!;
+		setReasons_final_value(a_t);
+	}
+
 	function showMainHintButton() {
 		return (
 			<div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
@@ -80,7 +210,7 @@ export default function Home() {
 	function renderHintButton(id: string, hint: string) {
 		return (
 			<button
-				title="_"
+				title="?"
 				type="button"
 				onClick={() => toggleSubHintView(id, hint)}
 				className="fixed left-0 top-0 flex justify-center pb-6 pt-8 backdrop-blur-2xl lg:static Ig:w-auto Lgrounded-xl lg:p-4 "
@@ -98,6 +228,87 @@ export default function Home() {
 					/>
 				</svg>
 			</button>
+		);
+	}
+
+	function renderCollectedDataType(ct: collected_type) {
+		return (
+			<Disclosure key={ct.name}>
+				{({ open }) => (
+					<div className="m-4">
+						<Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-1 px-4 py-2 text-left text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75">
+							<span>{ct.name + " : " + ct.option!.label}</span>
+							<ChevronUpIcon
+								className={`${
+									open ? "" : "rotate-180 transform"
+								} h-5 w-5 text-white`}
+							/>
+						</Disclosure.Button>
+
+						<Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
+							{ct.intension!.map((i: intension_type) => {
+								return (
+									<button
+										onClick={() => {
+											updateCollectedDataTypeIntension(ct, i.id, false);
+										}}
+										type="button"
+										className="mt-5 gap-4 w-full flex flex-row justify-start"
+										key={i.id}
+									>
+										<Checkbox
+											size="md"
+											color="default"
+											className="w-6 h-6 border-2"
+											isSelected={i.selected}
+											onValueChange={() => {
+												updateCollectedDataTypeIntension(ct, i.id, false);
+											}}
+										/>
+										<div className="">
+											<p className="h-auto w-auto | text-white text-start font-light">
+												<strong className="underlineunderline-offset-4">
+													{i.label}
+												</strong>
+											</p>
+										</div>
+									</button>
+								);
+							})}
+							<hr className="my-12 h-0.5 border-t-0 bg-neutral-100 dark:bg-white/10" />
+							{ct.purpose!.map((p: intension_type) => {
+								return (
+									<button
+										onClick={() => {
+											updateCollectedDataTypeIntension(ct, p.id, true);
+										}}
+										type="button"
+										className="mt-5 gap-4 w-full flex flex-row justify-start"
+										key={p.id}
+									>
+										<Checkbox
+											size="md"
+											color="default"
+											className="w-6 h-6 border-2"
+											isSelected={p.selected}
+											onValueChange={() => {
+												updateCollectedDataTypeIntension(ct, p.id, true);
+											}}
+										/>
+										<div className="">
+											<p className="h-auto w-auto | text-white text-start font-light">
+												<strong className="underlineunderline-offset-4">
+													{p.label}
+												</strong>
+											</p>
+										</div>
+									</button>
+								);
+							})}
+						</Disclosure.Panel>
+					</div>
+				)}
+			</Disclosure>
 		);
 	}
 
@@ -136,7 +347,7 @@ export default function Home() {
 											onChange={(e) => handleChange(index, e)}
 										/>
 										<button
-											title="_"
+											title="?"
 											type="button"
 											className="button remove"
 											onClick={() => removeTrackingDomains(index)}
@@ -167,6 +378,68 @@ export default function Home() {
 						</button>
 					</>
 				);
+			case "3":
+				return (
+					<div className="">
+						{reasons_final_value.map((type: api_type) => {
+							return (
+								<Disclosure key={type.id}>
+									{({ open }) => (
+										<div className="m-4">
+											<Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-1 px-4 py-2 text-left text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75">
+												<span>{type.type}</span>
+												<ChevronUpIcon
+													className={`${
+														open ? "" : "rotate-180 transform"
+													} h-5 w-5 text-white`}
+												/>
+											</Disclosure.Button>
+
+											<Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
+												{type.reasons.map((r: reason) => {
+													return (
+														<button
+															onClick={() => updateApiTypes(type.id, r.id)}
+															type="button"
+															className="mt-5 gap-4 w-full flex flex-row justify-start"
+															key={r.id}
+														>
+															<Checkbox
+																size="md"
+																color="default"
+																className="w-6 h-6 border-2"
+																isSelected={r.selected}
+																onValueChange={() =>
+																	updateApiTypes(type.id, r.id)
+																}
+															/>
+															<div className="">
+																<p className="h-auto w-auto | text-white text-start font-light">
+																	<strong className="underlineunderline-offset-4">
+																		{r.key}
+																	</strong>{" "}
+																	: {r.value}
+																</p>
+															</div>
+														</button>
+													);
+												})}
+											</Disclosure.Panel>
+										</div>
+									)}
+								</Disclosure>
+							);
+						})}
+					</div>
+				);
+			case "4":
+				return (
+					<div className=" w-auto h-auto flex flex-col gap-4">
+						{collected_data_types.map((cdt: collected_type) => {
+							return <>{renderCollectedDataType(cdt)}</>;
+						})}
+					</div>
+				);
 			default:
 				return (
 					<button
@@ -183,7 +456,16 @@ export default function Home() {
 	function renderForm() {
 		return (
 			<>
-				<div className=" grid grid-cols-2 gap-20 p-10 border-b border-blue-50 transition-all ease-in-out duration-700 opacity-100 h-auto mt-10 max-w-7xl w-full bg-gradient-to-r from-cyan-500 to-blue-500">
+				<div className=" z-10 max-w-5xl w-full items-end justify-end font-mono text-sm lg:flex">
+					<button
+						type="button"
+						onClick={toggle}
+						className="fixed  flex w-full justify-center border-b border-cyan-500 p-8 rounded-full backdrop-blur-2xl lg:static lg:w-auto g:rounded-xl lg:border lg:bg-black-200 1g:p-4 "
+					>
+						<strong> Download PrivacyInfo.xcprivacy</strong>
+					</button>
+				</div>
+				<div className=" grid grid-cols-2 gap-10 p-10 border-b border-blue-50 transition-all ease-in-out duration-700 opacity-100 h-auto mt-10 max-w-7xl w-full bg-gradient-to-r from-cyan-500 to-blue-500">
 					{datasource.map((data) => {
 						return (
 							<div key={data.id}>
